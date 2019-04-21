5ad34b137c70d5f227c85ea537ce801031380d44
KajdeMunter <kajdemunter@hotmail.com>
2019-04-21 19:55:36 +0200

    Either Monad and Co-routine attempt

---
+++ Code/Coroutine.ts
@@ -0,0 +1,57 @@
+import { Fun, id, Unit } from "./Fun"
+import { Either, inr, inl } from "./EitherMonad"
+import { Pair, map_Pair } from "./PairMonad"
+
+// The core idea of coroutines is that running the process will result in either one of three possible outomces:
+// A result, error on an interruption.
+
+// Same structure as Process. Instance of either a result (pair<a,s> containing both the final result and a state)
+// or something that is not a result (NoRes<s,e,a> is either an error or the rest of the process "continuation")
+// Which in turn is the current state of the process so far, and the coroutine that would perform the rest of the process.
+type Coroutine<s,e,a> = Fun<s, Either<NoRes<s,e,a>, Pair<a,s>>>
+type NoRes<s,e,a> = Either<e, Continuation<s,e,a>>
+type Continuation<s,e,a> = Pair<s,ContinuedCoroutine<s,e,a>>
+
+// Fix for circular reference that the example has. the rest of the code also doesn't work. 
+// Cant figure out why and I can't contact the teacher. Thanks. Very nice.
+interface ContinuedCoroutine<s,e,a> extends Coroutine<s,e,a> { }
+
+let unit_Co = <s,e,a>(x:a) : Coroutine<s,e,a> =>
+    Fun<s, Either<NoRes<s,e,a>, Pair<a,s>>>(s => inr<NoRes<s,e,a>, Pair<a,s>>().f({fst:x, snd:s}))
+
+let join_Co = <s,e,a> (): Fun<Coroutine<s,e,Coroutine<s,e,a>>, Coroutine<s,e,a>> => 
+    Fun(p => Fun(s => {
+        let res : Either<NoRes<s,e,Coroutine<s,e,a>>, Pair<Coroutine<s,e,a>,s>> = p.f(s)
+
+        if (res.kind == "left") {
+            if (res.value.kind == "left") {
+                // return inl().then(inl()).f(res.value.value)
+            } else {
+                let rest : Pair<s,Coroutine<s,e,Coroutine<s,e,a>>> = res.value.value
+                // return inl().then(inr()).then(map_Pair(id<s>(), join_Co()).f(rest))
+            }
+        } else {
+            // let final_res : Pair<s,Coroutine<s,e,a>> = res.value
+            return final_res.snd.f(final_res.fst)
+        }
+    }))
+
+
+// let map_Co = <s,e,a,b>(f:Fun<a,b>) : Fun<Coroutine<s,e,a>, Coroutine<s,e,b>> =>
+//     Fun(p => Fun(s => {
+//       let res : Either<NoRes<s,e,a>, Pair<a,s>> = p.f(s)
+//       if (res.kind == "left") {
+//         if (res.value.kind == "left") {
+//           return inl().then(inl()).f(res.value.value)
+//         } else {
+//           let rest : Pair<s,Coroutine<s,e,a>> = res.value.value
+//           return inl().then(inr()).then(map_Pair(id<s>(), map_Co(f)).f(rest))
+//         }
+//       } else {
+//           let final_res : Pair<s,a> = res.value
+//           return map_Pair(id<s>(), f).f(final_res)
+//       }
+//     }))
+
+// let suspend = <s,e>() : Coroutine<s,e,Unit> =>
+//     Fun(s => inl().inr().f({ x:unit_Co({}), y:s }))
---
+++ Code/EitherMonad.ts
@@ -0,0 +1,41 @@
+import { Fun } from "./Fun"
+
+export type Either<a, b> = {
+    kind: "left",
+    value: a
+} | {
+    kind: "right",
+    value: b
+}
+
+export let inl = <a, b>(): Fun<a, Either<a, b>> =>
+    Fun<a, Either<a, b>>((x: a) => ({
+      kind: "left",
+      value: x
+    }))
+
+  
+export let inr = <a, b>(): Fun<b, Either<a, b>> =>
+    Fun<b, Either<a, b>>((x: b) => ({
+        kind: "right",
+        value: x
+    }))
+
+export let map_Either = <a, a1, b, b1>(f: Fun<a, a1>, g: Fun<b, b1>): 
+  Fun<Either<a, b>, Either<a1, b1>> => {
+    return Fun((e: Either<a, b>): Either<a1, b1> => {
+      if (e.kind == "left") {
+        let newValue = f.f(e.value)
+        return inl<a1, b1>().f(newValue)
+      }
+      else {
+        let newValue = g.f(e.value)
+        return inr<a1, b1>().f(newValue)
+      }
+    }) // e.kind == "Left" ? f.then(inl()) : g.then(inr()) 
+}
+
+export let unit_Either = <a, b>() : Fun<a,Either<b,a>> => inr<b,a>()
+
+export let join_Either = <a, b>() : Fun<Either<b,Either<b,a>>, Either<b,a>> =>
+  Fun(x => x.kind == "left" ? inl<b,a>().f(x.value) : x.value)
