a280720116237fcdd394bb5b4d7d86214d195e3c
KajdeMunter <kajdemunter@hotmail.com>
2019-04-22 19:01:16 +0200

    Possible either bind implementation

--- Code/EitherMonad.ts
+++ Code/EitherMonad.ts
@@ -39,3 +39,8 @@ export let unit_Either = <a, b>() : Fun<a,Either<b,a>> => inr<b,a>()
 
 export let join_Either = <a, b>() : Fun<Either<b,Either<b,a>>, Either<b,a>> =>
   Fun(x => x.kind == "left" ? inl<b,a>().f(x.value) : x.value)
+
+export let bind_Either = <a, b, a1, b1>(left:Fun<a, Either<a, b>>, right:Fun<b, Either<a, b>>) : Fun<Either<a, b>, Either<a1, b1>> => 
+  Fun<Either<a, b>, Either<a1, b1>>((e:Either<a,b>) => 
+  map_Either<a, Either<a, b>, b, Either<a, b>>(left, right)
+  .then(join_Either<a, b>()).f(e))
