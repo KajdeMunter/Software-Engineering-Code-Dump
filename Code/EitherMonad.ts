5b33e1a714359a4eaae075a4368be5a6296334fa
KajdeMunter <kajdemunter@hotmail.com>
2019-04-23 12:35:41 +0200

    Either join and bind

--- Code/EitherMonad.ts
+++ Code/EitherMonad.ts
@@ -1,4 +1,4 @@
-import { Fun } from "./Fun"
+import { Fun, id } from "./Fun"
 
 export type Either<a, b> = {
     kind: "left",
@@ -37,10 +37,14 @@ export let map_Either = <a, a1, b, b1>(f: Fun<a, a1>, g: Fun<b, b1>):
 
 export let unit_Either = <a, b>() : Fun<a,Either<b,a>> => inr<b,a>()
 
-export let join_Either = <a, b>() : Fun<Either<b,Either<b,a>>, Either<b,a>> =>
-  Fun(x => x.kind == "left" ? inl<b,a>().f(x.value) : x.value)
 
-export let bind_Either = <a, b, a1, b1>(left:Fun<a, Either<a, b>>, right:Fun<b, Either<a, b>>) : Fun<Either<a, b>, Either<a1, b1>> => 
-  Fun<Either<a, b>, Either<a1, b1>>((e:Either<a,b>) => 
-  map_Either<a, Either<a, b>, b, Either<a, b>>(left, right)
-  .then(join_Either<a, b>()).f(e))
+export let join_Either = <a, b>() : Fun<Either<b,Either<b,a>>, Either<b,a>> =>
+  Fun(x => x.kind === "right" ? x.value : inl<b,a>().f(x.value))
+
+export let join_Either_left = <a, b>() : Fun<Either<Either<a, b>, b>, Either<a, b>> => 
+  Fun(x => x.kind === "left" ? x.value : inr<a,b>().f(x.value))
+
+export let bind_Either_right = <a, b, c>(f:Fun<b, Either<a, c>>) : Fun<Either<a, b>, Either<a, c>> => 
+  Fun<Either<a, b>, Either<a, c>>((e:Either<a,b>) => 
+  map_Either<a, a, b, Either<a, c>>(id<a>(), f)
+  .then(join_Either()).f(e))
