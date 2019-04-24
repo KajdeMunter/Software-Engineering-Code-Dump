023ee93f143363632b166b0ff14db21e8105ec82
KajdeMunter <kajdemunter@hotmail.com>
2019-04-24 13:11:49 +0200

    option bind and join

--- Code/Coroutine.ts
+++ Code/Coroutine.ts
@@ -8,13 +8,22 @@ import { Pair, map_Pair } from "./PairMonad"
 // Same structure as Process. Instance of either a result (pair<a,s> containing both the final result and a state)
 // or something that is not a result (NoRes<s,e,a> is either an error or the rest of the process "continuation")
 // Which in turn is the current state of the process so far, and the coroutine that would perform the rest of the process.
-type Coroutine<s,e,a> = Fun<s, Either<NoRes<s,e,a>, Pair<a,s>>>
-type NoRes<s,e,a> = Either<e, Continuation<s,e,a>>
+
+type Coroutine<s,e,a> = Fun<s, Either<NoRes<s,e,a>,Pair<a,s>>>
+type NoRes<s,e,a> = Either<e,Continuation<s,e,a>>
 type Continuation<s,e,a> = Pair<s,ContinuedCoroutine<s,e,a>>
 
 // Fix for circular reference that the example has. the rest of the code also doesn't work. 
 // Cant figure out why and I can't contact the teacher. Thanks. Very nice.
 interface ContinuedCoroutine<s,e,a> extends Coroutine<s,e,a> { }
+
+// type Co<s,e,a> = Fun<s, Either<NoRes<s,e,a>,Pair<a,s>>>
+// type Continuation<s,e,a> = Pair<s,Coroutine<s,e,a>>
+// type NoRes<s,e,a> = Either<e,Continuation<s,e,a>>
+
+// interface Coroutine<s, e, a> {
+//     run: Co<s, e, a>
+// }
 
 let unit_Co = <s,e,a>(x:a) : Coroutine<s,e,a> =>
     Fun<s, Either<NoRes<s,e,a>, Pair<a,s>>>(s => inr<NoRes<s,e,a>, Pair<a,s>>().f({fst:x, snd:s}))
@@ -25,14 +34,14 @@ let join_Co = <s,e,a> (): Fun<Coroutine<s,e,Coroutine<s,e,a>>, Coroutine<s,e,a>>
 
         if (res.kind == "left") {
             if (res.value.kind == "left") {
-                // return inl().then(inl()).f(res.value.value)
+                return inl().then(inl()).f(res.value.value)
             } else {
-                let rest : Pair<s,Coroutine<s,e,Coroutine<s,e,a>>> = res.value.value
-                // return inl().then(inr()).then(map_Pair(id<s>(), join_Co()).f(rest))
+                let rest:Pair<s,ContinuedCoroutine<s,e,ContinuedCoroutine<s,e,a>>> = res.value.value
+                return inl().then(inr()).then(map_Pair(id<s>(), join_Co<s, e, a>())).f(rest)
             }
         } else {
-            // let final_res : Pair<s,Coroutine<s,e,a>> = res.value
-            return final_res.snd.f(final_res.fst)
+            let final_res : Pair<Coroutine<s,e,a>, s> = res.value
+            return final_res.fst.f(final_res.snd)
         }
     }))
 
--- Code/OptionMonad.ts
+++ Code/OptionMonad.ts
@@ -7,8 +7,6 @@ type Option<a> = {
     kind: "none"
 }
 
-let bind_Option = function <a,b>(this: Option<a>, k: Fun<a, Option<b>>) : Option<b> {return null!}
-
 let None = <a>():Option<a> => ({kind:"none"})
 
 let Some = <a>():Fun<a, Option<a>> => Fun<a, Option<a>>(x => ({ kind:"some", value:x }))
@@ -19,3 +17,9 @@ let map_Option = <a,b>(f:Fun<a,b>) : Fun<Option<a>, Option<b>> =>
     : f.then(Some<b>()).f(x.value))
 
 let unit_Option = <a>() : Fun<a, Option<a>> => Fun(a => None<a>())
+
+let join_Option = <a>() : Fun<Option<Option<a>>, Option<a>> =>
+    Fun(o_o => o_o.kind === "none" ? None<a>() : o_o.value)
+
+let bind_Option = <a,b>(o: Option<a>, k: Fun<a, Option<b>>) : Option<b> =>
+    map_Option(k).then(join_Option()).f(o)
